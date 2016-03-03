601083ec1fa137985a5888e273478d090b2d4643
Julien M <julien@moscw.com>
2016-03-03 17:50:11 -0300

    Check HTTPS and make Localhost URL work via .xip.io.

--- app/scripts/overlay.js
+++ app/scripts/overlay.js
@@ -11,22 +11,51 @@ overlayId = 'chromeextensionresizeriframeoverlay';
 // - 'handset': mobile
 device = 'window';
 
+// give back a *.xip.io URL if given URL is detected as 'local'
+function checkUrl(url) {
+  // 'https://localhost:3000/jlong/2428561?foo=bar#test'
+  var parsedUrl = new URL(url);
+
+  // check if we're on a HTTPS page
+  if (parsedUrl.protocol == 'https:') {
+    console.log('Sorry, this extension doesn\'t (yet) work on HTTPS pages :(');
+    return false;
+  }
+
+  // check if we're on a localhost page
+  if (parsedUrl.hostname == 'localhost' || parsedUrl.hostname == '127.0.0.1') {
+    // special case for 'localhost'
+    if (parsedUrl.hostname == 'localhost') {
+      parsedUrl.hostname = '127.0.0.1';
+    }
+
+    // append .xip.io
+    parsedUrl.hostname = parsedUrl.hostname + '.xip.io';
+  }
+
+  // return full, modified URL
+  return parsedUrl.href;
+}
+
 // Create an iframe and load Resizer within
 function createOverlay() {
-  $('<iframe>', {
-    src: 'http://g.co/design/resizer#device=' + device + '&url=' + encodeURI(url),
-    id:  overlayId,
-    frameborder: 0,
-    scrolling: 'yes',
-  })
-  .css({
-    height: $(window).height(),
-    width: $(window).width(),
-    position: 'fixed',
-    top: '0',
-    'z-index': '10000'
-  })
-  .appendTo('body');
+  // check URL, adapt it for local dev if required
+  if (correctUrl = checkUrl(url)) {
+    $('<iframe>', {
+      src: 'http://g.co/design/resizer#device=' + device + '&url=' + encodeURI(correctUrl),
+      id:  overlayId,
+      frameborder: 0,
+      scrolling: 'yes',
+    })
+    .css({
+      height: $(window).height(),
+      width: $(window).width(),
+      position: 'fixed',
+      top: '0',
+      'z-index': '10000'
+    })
+    .appendTo('body');
+  }
 }
 
 // Anonymous "self-invoking" function
